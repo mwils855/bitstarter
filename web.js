64a962b9be7c09f9512ce4a78426efe851a53424
Mike Wilson <mwils855@gmail.com>
2013-08-24 01:51:02 +0000

    Update web.js with html 1st try

--- web.js
+++ web.js
@@ -3,10 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(fs.readFileSync(index.html))};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  console.log("Listening on " + port)};
