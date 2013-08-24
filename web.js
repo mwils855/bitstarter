b890a34ba756c61325b60c35603b20178eff1c03
Mike Wilson <mwils855@gmail.com>
2013-08-24 02:22:01 +0000

    Back to normal

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html))};
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port)};
+  console.log("Listening on " + port);
+});
