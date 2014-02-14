5f2daa475d47ecec6b88206fad65be2c3eda661c
Mike Wilson <mwils855@gmail.com>
2014-02-14 03:55:23 +0000

    changed web.js to use index2.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html','utf8'));
+  response.send(fs.readFileSync('index2.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;
